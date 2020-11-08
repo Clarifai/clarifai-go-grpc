267226c9998b87aa94496979aacb418d80a5d69b
Rok Povsic <rok.povsic@gmail.com>
2020-11-08 09:19:50 +0100

    Add a test predicting on a file

--- integration_test.go
+++ integration_test.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -57,6 +58,34 @@ func TestPostModelOutputsWithUrl(t *testing.T) {
 					Data: &api.Data{
 						Image: &api.Image{
 							Url: DogImageUrl}}}}})
+	if err != nil {
+		panic(err)
+	}
+	assertSuccessResponse(t, response.Status)
+
+	if len(response.Outputs[0].Data.Concepts) == 0 {
+		t.Errorf("Received no outputs")
+	}
+}
+
+func TestPostModelOutputsWithFileBytes(t *testing.T) {
+	client := makeClient()
+	ctx := makeContext()
+
+	fileBytes, err := ioutil.ReadFile("test_assets/red-truck.png")
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.PostModelOutputs(
+		ctx,
+		&api.PostModelOutputsRequest{
+			ModelId: GeneralModelId,
+			Inputs: []*api.Input{
+				{
+					Data: &api.Data{
+						Image: &api.Image{
+							Base64: fileBytes}}}}})
 	if err != nil {
 		panic(err)
 	}
