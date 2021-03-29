e5f946e52d9a3cb87168ad58aa6392bab471c0ce
aleksander-clarifai <aleksander.belov@clarifai.com>
2021-03-29 16:44:06 +0300

    Version 7.3.0

--- proto/clarifai/api/service_grpc.pb.go
+++ proto/clarifai/api/service_grpc.pb.go
@@ -4,7 +4,7 @@ package api
 
 import (
 	context "context"
-	status "github.com/Clarifai/clarifai-go-grpc/proto/clarifai/api/status"
+	status "github.com/Clarifai/clarifai-go-grpc/proto/clarifai/api/github.com/Clarifai/clarifai-go-grpc/proto/clarifai/api/status"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status1 "google.golang.org/grpc/status"
