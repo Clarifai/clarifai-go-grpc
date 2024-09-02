b0152b18e32c68b90720b0ffee118744c70ebb18
clarifai-prod <clarifai-prod@users.noreply.github.com>
2024-09-02 12:07:55 +0000

    GRPC clients version 10.8.0

--- proto/clarifai/api/service_grpc.pb.go
+++ proto/clarifai/api/service_grpc.pb.go
@@ -257,6 +257,7 @@ const (
 	V2_PostDeployments_FullMethodName                       = "/clarifai.api.V2/PostDeployments"
 	V2_PatchDeployments_FullMethodName                      = "/clarifai.api.V2/PatchDeployments"
 	V2_DeleteDeployments_FullMethodName                     = "/clarifai.api.V2/DeleteDeployments"
+	V2_PostAuditLogSearches_FullMethodName                  = "/clarifai.api.V2/PostAuditLogSearches"
 )
 
 // V2Client is the client API for V2 service.
@@ -848,6 +849,7 @@ type V2Client interface {
 	PatchDeployments(ctx context.Context, in *PatchDeploymentsRequest, opts ...grpc.CallOption) (*MultiDeploymentResponse, error)
 	// Delete multiple deployments in one request.
 	DeleteDeployments(ctx context.Context, in *DeleteDeploymentsRequest, opts ...grpc.CallOption) (*status.BaseResponse, error)
+	PostAuditLogSearches(ctx context.Context, in *PostAuditLogSearchesRequest, opts ...grpc.CallOption) (*MultiAuditLogSearchResponse, error)
 }
 
 type v2Client struct {
@@ -3319,6 +3321,16 @@ func (c *v2Client) DeleteDeployments(ctx context.Context, in *DeleteDeploymentsR
 	return out, nil
 }
 
+func (c *v2Client) PostAuditLogSearches(ctx context.Context, in *PostAuditLogSearchesRequest, opts ...grpc.CallOption) (*MultiAuditLogSearchResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(MultiAuditLogSearchResponse)
+	err := c.cc.Invoke(ctx, V2_PostAuditLogSearches_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // V2Server is the server API for V2 service.
 // All implementations must embed UnimplementedV2Server
 // for forward compatibility
@@ -3908,6 +3920,7 @@ type V2Server interface {
 	PatchDeployments(context.Context, *PatchDeploymentsRequest) (*MultiDeploymentResponse, error)
 	// Delete multiple deployments in one request.
 	DeleteDeployments(context.Context, *DeleteDeploymentsRequest) (*status.BaseResponse, error)
+	PostAuditLogSearches(context.Context, *PostAuditLogSearchesRequest) (*MultiAuditLogSearchResponse, error)
 	mustEmbedUnimplementedV2Server()
 }
 
@@ -4625,6 +4638,9 @@ func (UnimplementedV2Server) PatchDeployments(context.Context, *PatchDeployments
 }
 func (UnimplementedV2Server) DeleteDeployments(context.Context, *DeleteDeploymentsRequest) (*status.BaseResponse, error) {
 	return nil, status1.Errorf(codes.Unimplemented, "method DeleteDeployments not implemented")
+}
+func (UnimplementedV2Server) PostAuditLogSearches(context.Context, *PostAuditLogSearchesRequest) (*MultiAuditLogSearchResponse, error) {
+	return nil, status1.Errorf(codes.Unimplemented, "method PostAuditLogSearches not implemented")
 }
 func (UnimplementedV2Server) mustEmbedUnimplementedV2Server() {}
 
@@ -8932,6 +8948,24 @@ func _V2_DeleteDeployments_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _V2_PostAuditLogSearches_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PostAuditLogSearchesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(V2Server).PostAuditLogSearches(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: V2_PostAuditLogSearches_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(V2Server).PostAuditLogSearches(ctx, req.(*PostAuditLogSearchesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // V2_ServiceDesc is the grpc.ServiceDesc for V2 service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -9870,6 +9904,10 @@ var V2_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "DeleteDeployments",
 			Handler:    _V2_DeleteDeployments_Handler,
+		},
+		{
+			MethodName: "PostAuditLogSearches",
+			Handler:    _V2_PostAuditLogSearches_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
